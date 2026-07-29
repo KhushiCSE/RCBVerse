52a2496a0c3b4b8dcf546db1899c19ebd75ada01
KhushiCSE <khushikrishna3010@gmail.com>
2026-07-29 14:21:05 +0530

    FIIIX

--- Navbar.tsx
+++ Navbar.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Trophy, Volume2, VolumeX, Wind, Music, PartyPopper, Megaphone, Radio, X } from 'lucide-react';
 import type { ModuleId } from './modules';
-import type { NAV_MODULES } from './modules';
+import { NAV_MODULES } from './modules';
 import { useAudio } from './audioStore';
 
 interface NavbarProps {
